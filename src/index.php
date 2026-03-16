3bc0279b6c3b916001dbc67bf22dc64036594282
nico9707190 <frascanni07@gmail.com>
2026-03-16 11:06:54 +0100

    Tailwind CSS

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sky&Go - Prenota e Acquista voli </title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div class="hero1">
